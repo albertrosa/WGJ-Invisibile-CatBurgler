04c1bb405476b90215d994669a88a0bda819652b
AlbertRosa <rosalbert@gmail.com>
2020-05-02 22:17:19 -0400

    Added Cloak Dissolve

---
+++ Assets/Scripts/Dissolve.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    Material material;
+
+    bool isDissolving = false;
+    float fade = 1f;
+
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cloak"))
+        {
+            isDissolving = true;
+        } else if (Input.GetButtonUp("Cloak") ) {
+            isDissolving = false;
+        }
+
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+
+            if (fade <= 0)
+            {
+                fade = 0;
+            }
+        } else
+        {
+            fade += Time.deltaTime;
+
+            if (fade >= 1)
+            {
+                fade = 1;
+            }
+        }
+
+        material.SetFloat("_Fade", fade);
+
+    }
+}
